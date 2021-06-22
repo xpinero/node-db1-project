3ad126b66a761f3e83f3a9d5ca91d2cfc3bd927d
Xaver Pinero <xaver.pinero@gmail.com>
2021-06-21 21:28:04 -0400

    created middleware and models

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -1,27 +1,48 @@
-const getAll = () => {
-  // DO YOUR MAGIC
-}
-
-const getById = id => {
-  // DO YOUR MAGIC
-}
-
-const create = account => {
-  // DO YOUR MAGIC
-}
-
-const updateById = (id, account) => {
-  // DO YOUR MAGIC
-}
-
-const deleteById = id => {
-  // DO YOUR MAGIC
-}
+const db = require('../../data/db-config');
 
 module.exports = {
   getAll,
   getById,
+  getUniqueName,
   create,
   updateById,
   deleteById,
+}
+
+
+function getAll() {
+  // DO YOUR MAGIC
+  return db ('accounts');
+}
+
+function getById(id) {
+  // DO YOUR MAGIC
+  return db('accounts')
+    .where({ id })
+    .first();
+}
+
+function getUniqueName(name) {
+  return db('accounts')
+    .where({name})
+    .first();
+}
+
+async function create({name,budget}){
+  // DO YOUR MAGIC
+  const [account] = await db("accounts").insert({name,budget})
+  return getById(account)
+}
+
+async function updateById(id, {name, budget}) {
+  // DO YOUR MAGIC
+  await db("accounts").where("id", id).update({name, budget})
+  return getById(id)
+}
+
+async function deleteById(id) {
+  // DO YOUR MAGIC
+  const deleteAccount = await getById(id)
+  await db("accounts").where("id",id).delete()
+  return deleteAccount
 }
